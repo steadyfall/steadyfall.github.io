5d4908c114d410d639178789723a33f15a9739ad
Himank Dave <hddave@uwaterloo.ca>
2024-09-29 20:13:14 -0400

    made scroll progress bar (on top of page)

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,19 @@
+import useReadingProgress from '../hooks/useReadingProgress'
+
+const Navbar = () => {
+    const completion = useReadingProgress();
+
+    return (
+        <nav className="py-0.5 sticky top-0">
+            {/* <div className="flex items-center justify-between container mx-auto text-xl"></div> */}
+            <div className="h-0.5 overflow-hidden">
+                <span
+                    className="absolute bg-midnight-blue-600 dark:bg-midnight-blue-400 h-full w-full top-0 bottom-0"
+                    style={{ transform: `translateX(${completion - 100}%)` }}
+                />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
