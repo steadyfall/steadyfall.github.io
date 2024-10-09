01ea157333a18955187f48772732bd80a2b00970
Himank Dave <hddave@uwaterloo.ca>
2024-10-08 20:48:42 -0400

    feat: added Space Mono font as typewriter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google'
+import { Inter, Space_Mono } from 'next/font/google'
 import { Providers } from './providers'
 import "./globals.css";
 
 const inter = Inter({ subsets: ['latin'] })
+const spaceMono = Space_Mono({ 
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-spaceMono'
+})
 
 export const metadata: Metadata = {
   title: {
@@ -18,8 +23,9 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // ${inter.className} 
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en" className={`${inter.className} ${spaceMono.variable}`} suppressHydrationWarning>
       <body>
         <div className={`min-h-screen`}>
             <Providers> {children} </Providers>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-spaceMono)'],
+        typewriter: ['var(--font-spaceMono)'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
