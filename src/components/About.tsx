0f2d21293fc93e3c242ed649d5d0e4d40a03b53d
Himank Dave <hddave@uwaterloo.ca>
2024-10-08 20:47:18 -0400

    chore: fix About section and update favicon

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,14 +1,31 @@
+import { ArrowUpRight } from "lucide-react"
+
 const About = () => {
   return (
     <section id="about" className="mb-12">
         <h2 className="text-2xl font-bold mb-4">About</h2>
         <p className="text-gray-700 dark:text-gray-300">
-            I am a third-year Computational Mathematics student at the University of Waterloo.
+            I am a junior at the University of Waterloo, pursuing a major in Computational Mathematics.
             With a passion for technology and innovation, I am constantly exploring new
             areas in software development and artificial intelligence. 
             I am eager to work in areas that leverage the combined potential of machine learning and
-            software development, regardless of traditional boundaries. Outside of academics,
-            I enjoy contributing to open-source projects and participating in hackathons. 
+            software development, regardless of traditional boundaries. <br></br>
+            Outside of academics and professional career, I enjoy contributing to open-source projects, working on side projects, participating in hackathons and playing 
+            table tennis, football, cricket and badminton. I also like to travel & explore new places, cuisines & restaurants.<br></br>
+            You can find me re-watching any one of these TV series:
+              Suits,
+              The Office,
+              Brooklyn 9-9,
+              How I Met Your Mother,
+              The Big Bang Theory,
+              South Park,
+              Impractical Jokers.
+            <br></br>
+            For those of you curious, the favicon is a 4-hypercube graph. Check the <a href="/icon.ico" target="_blank" className="inline-flex items-center hover:underline">
+              image <ArrowUpRight className="w-4 h-4 md:w-5 md:h-5"/>
+            </a> out and learn <a href="https://en.wikipedia.org/wiki/Hypercube_graph" target="_blank" className="inline-flex items-center hover:underline">
+              more <ArrowUpRight className="w-4 h-4 md:w-5 md:h-5"/>
+            </a>.
         </p>
     </section>
   )
