332b49f6daf07b01f6da831c990e9e328e7ff2b0
Himank Dave <hddave@uwaterloo.ca>
2024-11-08 19:07:23 -0500

    feat: revamp footer

--- site/src/components/Footer.tsx
+++ site/src/components/Footer.tsx
@@ -1,14 +1,31 @@
-import { Heart } from "lucide-react"
+import { ArrowUpRight } from "lucide-react"
+import { detailsForMetadata, personalDetails } from "@/app/config"
 
 const Footer = () => {
   return (
-    <footer className="mt-2 py-6 text-center text-gray-500 dark:text-gray-400">
-        <p>
-          Designed & built with <span className="inline-flex items-center">
-            <Heart className="w-3 h-3 md:w-4 md:h-4" color="#ff4444"/>
-          </span> by <span className="underline underline-offset-4">Himank Dave</span>. You can take inspirations from this or fork it, as long as you credit me.
-        </p>
+    <footer className="py-16 text-gray-500 dark:text-gray-400 flex flex-col items-center justify-center sm:flex-row-reverse sm:justify-between">
+      <div className="flex justify-center md:justify-start space-x-4">
+          <a href={personalDetails.githubLink} target="_blank" rel="noopener noreferrer" aria-label="GitHub"
+          className="inline-flex items-center hover:underline hover:underline-offset-2 text-pink-500"
+          >
+          github <ArrowUpRight className="w-4 h-4 md:w-5 md:h-5"/>
+          </a>
+          <a href={personalDetails.linkedinLink} target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"
+          className="inline-flex items-center hover:underline hover:underline-offset-2 text-thunderbird-600"
+          >
+          linkedin <ArrowUpRight className="w-4 h-4 md:w-5 md:h-5"/>
+          </a>
+          <a href={`mailto:` + personalDetails.email} aria-label="Email"
+          className="inline-flex items-center hover:underline hover:underline-offset-2 text-azure-radiance-600" // text-orange-500"
+          >
+          email <ArrowUpRight className="w-4 h-4 md:w-5 md:h-5"/>
+          </a>
+      </div>
+      <div className="mt-6 sm:mt-0">
+        &copy; 2024 {new URL(detailsForMetadata.baseUrl).hostname}
+      </div>
     </footer>
+    
   )
 }
 
