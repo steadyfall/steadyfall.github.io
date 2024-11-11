e88da5e11153110b4ae6444aaeadd19b42b65c5c
Himank Dave <hddave@uwaterloo.ca>
2024-11-11 06:01:32 -0500

    feat: added LinkWithArrow component for consistent animation across links with arrows

---
+++ src/components/ui/LinkWithArrow.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { ArrowUpRight } from 'lucide-react'
+
+
+export default function LinkWithArrow({ className, href, children, ...props }: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
+  return (
+    <a
+      className={
+        cn("group inline-flex items-center", className)
+      }
+      href={href}
+      {...props}
+    >
+        {children}
+        <ArrowUpRight className="w-4 h-4 md:w-5 md:h-5 transition-transform duration-300 group-hover:translate-x-1 group-hover:-translate-y-1"/>
+    </a>
+  );
+}
