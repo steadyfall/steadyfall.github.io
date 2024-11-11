ad4b2020082ac672f0c89f536e7717032ce44328
Himank Dave <hddave@uwaterloo.ca>
2024-11-11 05:00:24 -0500

    refactor: remove placeholder for theme toggler

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -11,17 +11,7 @@ export default function ThemeSwitch() {
 
   useEffect(() =>  setMounted(true), [])
 
-  if (!mounted) return (
-    <img
-      src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-      width={36}
-      height={36}
-      sizes="36x36"
-      alt="Loading Light/Dark Toggle"
-      title="Loading Light/Dark Toggle"
-    />
-    // priority={false}
-  )
+  if (!mounted) return null;
 
   return (
     <button
