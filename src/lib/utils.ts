60cb913542e5b8bad4bd1d6c5825237b86898b5a
Himank Dave <hddave@uwaterloo.ca>
2024-11-13 02:53:25 -0500

    style: added function to format date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const formattedDate = (d: Date): string => {
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: '2-digit',
+    year: 'numeric'
+  }).format(d);
+};
