acc5c4713b6d645834addd75aba7081d593cea3c
Himank Dave <hddave@uwaterloo.ca>
2024-12-18 09:11:28 -0500

    feat: made navbar highlight on hover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import { cn } from '@/lib/utils';
 import ThemeSwitch from './ThemeSwitch';
+import HoverHighlight from '@/components/ui/HoverHighlighter';
 import Link from 'next/link';
 
 const Navbar = () => {
@@ -14,10 +15,10 @@ const Navbar = () => {
                 "max-w-full mx-auto px-8 sm:px-16 md:px-24 lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl"
             )}>
                 <ul className="flex gap-4 sm:gap-8">
-                    <li><Link href="/" className="hover:underline hover:underline-offset-4">home</Link></li>
-                    <li><Link href="/#experience" className="hover:underline hover:underline-offset-4">experience</Link></li>
-                    <li><Link href="/#projects" className="hover:underline hover:underline-offset-4">projects</Link></li>
-                    <li><Link href="/posts" className="hover:underline hover:underline-offset-4">blog</Link></li>
+                    <li><Link href="/"><HoverHighlight text="home" /></Link></li>
+                    <li><Link href="/#experience"><HoverHighlight text="experience"/></Link></li>
+                    <li><Link href="/#projects"><HoverHighlight text="projects"/></Link></li>
+                    <li><Link href="/posts"><HoverHighlight text="blog"/></Link></li>
                 </ul>
                 <div className="flex gap-0 sm:gap-4">
                     <ThemeSwitch />
