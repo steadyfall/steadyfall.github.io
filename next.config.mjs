b71762c6ac4f44d0d2fd7516e495efb72487b09b
Himank Dave <hddave@uwaterloo.ca>
2025-01-31 21:57:40 -0500

    build: added mdx-js to render mdx files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-};
+const nextConfig = {};
 
 export default nextConfig;
+
+// import withMDX from '@next/mdx';
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export',
+//     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+// };
+
+// export default withMDX()(nextConfig);
